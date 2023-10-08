74636ab03ba4c36d02ee157b36168b224964a73f
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-08 10:11:34 +0000

    user追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  has_one_attached :inage
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   root 'homes#top'
   
   resources :books, only: [:new, :create, :index, :show, :destroy]

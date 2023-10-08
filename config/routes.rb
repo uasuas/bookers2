13b33c5467f61f31368b1da7c6957713ed09a362
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-08 13:41:06 +0000

    routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/home/about' => "homes#about", as: "about"
 
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

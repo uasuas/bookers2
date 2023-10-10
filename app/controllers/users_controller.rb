de5822f12a401b032edebeb9f4cfd5a26699c1a2
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-10 06:07:26 +0000

    users/id show~books/id show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  
-  
+
+
   def new
     @user = User.new
   end
@@ -9,31 +9,32 @@ class UsersController < ApplicationController
     @users = User.all
     # @user = User.find(params[:id])
     @book = Book.new
-    
+
   end
 
   def show
     @user = User.find(params[:id])
-    @book = Book.new
+    @book = Book.find(params[:id])
+    @books = @user.books
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
-  
+
+
   # def is_matching_login_user
   #   user = User.find(params[:id])
   #   unless user.id == current_user.id

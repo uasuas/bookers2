3f3f1461a07fbfdd91ebab6269144bbf8b97d801
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-16 03:50:02 +0000

    いいね機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 
   get '/home/about' => "homes#about", as: "about"
 
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only: [:new, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

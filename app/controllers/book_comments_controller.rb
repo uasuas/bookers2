af51a9ce560620e033a25f47bd089d368ae2506e
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-17 10:37:24 +0000

    コメント非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,17 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    if comment.save
-      flash[:notice] = "successfully"
-      redirect_to book_path(book)
-    else
-      flash[:notice] = "コメント入力してね！！"
-      redirect_to request.referer
-    end
+    comment.book_id = @book.id
+    comment.save
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    # redirect_to book_path(params[:book_id])
   end
 
   private

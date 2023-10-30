3a462011bf56d658bb3bfca3cf279f22e9eaa9cc
usausa <143808163+uasuas@users.noreply.github.com>
2023-10-30 02:51:47 +0000

    グループ入退室実装

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,9 @@ Rails.application.routes.draw do
   # DM機能
   resources :chats, only: [:show, :create, :destroy]
   # グループ機能
-  resources :groups, except: [:destroy]
+  resources :groups, only:  [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

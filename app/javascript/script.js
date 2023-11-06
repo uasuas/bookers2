e4edc5e42152c3224973bf28f9b31948249b1adc
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-06 11:06:00 +0000

    JS,usernewbook設定仮

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -17,7 +17,7 @@ $(function() {
   $('#back a').on('click',function(event){
     $('body, html').animate({
       scrollTop:0
-    }, 800);
+    }, 1000);
     event.preventDefault();
   });
 });
@@ -37,3 +37,27 @@ const opt = {
 $(document).on('turbolinks:load', function() {
     let swiper = new Swiper('.swiper',opt);
 });
+
+$(function(){
+  $('#partial-template').on('click',function(){
+    $('#partial-template').css({
+      'display': 'none',
+    }).slideUp();
+  });
+});
+
+// // イベントを監視する要素を取得
+// const targetElement = document.getElementById('target-element'); // マウスオーバーをトリガーする要素
+// const partialTemplate = document.getElementById('partial-template'); // 部分テンプレートの要素
+// const closeButton = document.getElementById('close-partial-template'); // 部分テンプレートを閉じるボタン
+
+// // マウスオーバー時の処理
+// targetElement.addEventListener('mouseover', () => {
+//   // 部分テンプレートを表示
+//   partialTemplate.style.display = 'block';
+// });
+
+// // 部分テンプレートを閉じる処理
+// closeButton.addEventListener('click', () => {
+//   partialTemplate.style.display = 'none';
+// });

982913598c8c7465cfc26108e0bbef37783026fa
usausa <143808163+uasuas@users.noreply.github.com>
2023-11-30 01:52:24 +0000

    コメントにフラッシュ非同期実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -4,13 +4,18 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = @book.id
-    comment.save
+    if comment.save
+      flash.now[:notice] = "コメントを投稿しました。"
+    else
+      flash.now[:alert] = "コメントを入力してください。"
+    end
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
     @book = Book.find(params[:book_id])
     @book_comment = BookComment.new
+    flash.now[:notice] = "コメントを削除しました。"
     # redirect_to book_path(params[:book_id])
   end
 
